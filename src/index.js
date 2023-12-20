51f44fc6b9af4411ba40ca3d4ed8f94a572555bb
Cornelius Soon <cornelius.soon@gmail.com>
2023-12-21 12:09:44 +1300

    Setup connection between logic and DOM layer

--- src/index.js
+++ src/index.js
@@ -6,5 +6,21 @@ import Inputs from "./javascript/models/inputs";
     const inputs = new Inputs(formData);
     const form = new Form(formData, inputs);
 
+    const fragment = document.createDocumentFragment();
+
+    form.inputs.forEach((input) => {
+        const component = document.createElement("div");
+
+        const inputElement = document.createElement("input");
+        inputElement.type = input.type;
+        inputElement.textContent = input.name;
+        inputElement.id = input.id;
+
+        component.appendChild(inputElement);
+        fragment.appendChild(component);
+    });
+
+    document.body.appendChild(fragment);
+
     return {};
 })();
