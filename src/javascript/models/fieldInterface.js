4ebaca4c15f5e8975f66ac0329403c6a11397d62
Cornelius Soon <cornelius.soon@gmail.com>
2023-12-27 14:31:41 +1300

    Add FieldInterface object

---
+++ src/javascript/models/fieldInterface.js
@@ -0,0 +1,19 @@
+import Input from "./input";
+
+class FieldInterface {
+    constructor(elementType) {
+        this.constructorType = FieldInterface.getConstructorType(elementType);
+    }
+
+    static getConstructorType(elementType) {
+        const constructors = {
+            input: Input,
+            // select: Select,
+            // textarea: Textarea,
+        };
+
+        return constructors[elementType];
+    }
+}
+
+export default FieldInterface;
