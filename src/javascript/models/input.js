a5df9b2f13dc9a2549334c6e10c19f8b6c4c45b9
Cornelius Soon <cornelius.soon@gmail.com>
2023-12-28 13:59:54 +1300

    Remove validator responsibility

--- src/javascript/models/input.js
+++ src/javascript/models/input.js
@@ -1,12 +1,9 @@
-import Validators from "./validators";
-
 class Input {
-    constructor(inputDataObject, validatorsDataObject) {
+    constructor(inputDataObject) {
         this.element = inputDataObject.element;
         this.type = inputDataObject.type;
         this.id = inputDataObject.id;
         this.name = inputDataObject.name;
-        this.validators = new Validators(validatorsDataObject).validators;
     }
 }
 
