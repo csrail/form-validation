6eb923a08c9b94386377253e94153869188edc6d
Cornelius Soon <cornelius.soon@gmail.com>
2023-12-29 11:08:01 +1300

    Add options property to Select model

--- src/javascript/models/select.js
+++ src/javascript/models/select.js
@@ -1,6 +1,9 @@
+import Options from "./options";
+
 class Select {
     constructor(selectDataObject) {
         this.element = selectDataObject.element;
+        this.options = new Options(selectDataObject.options).options;
     }
 }
 
