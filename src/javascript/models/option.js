e31809783194489f2cc27eb83c6a5a19ae128638
Cornelius Soon <cornelius.soon@gmail.com>
2023-12-29 11:07:23 +1300

    Add Option model

---
+++ src/javascript/models/option.js
@@ -0,0 +1,8 @@
+class Option {
+    constructor(optionDataObject) {
+        this.description = optionDataObject.description;
+        this.value = optionDataObject.value;
+    }
+}
+
+export default Option;
